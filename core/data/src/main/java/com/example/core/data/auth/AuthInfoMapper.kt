0213819efdb3358ade9069661d481f16e711ff27
Johan Aponte <johan.daniel.aponte94@gmail.com>
2025-11-27 13:01:37 -0500

    Git Test

--- core/data/src/main/java/com/example/core/data/auth/AuthInfoMapper.kt
+++ core/data/src/main/java/com/example/core/data/auth/AuthInfoMapper.kt
@@ -2,6 +2,7 @@ package com.example.core.data.auth
 
 import com.example.core.domain.AuthInfo
 
+
 fun AuthInfo.toAuthInfoSerializable(): AuthInfoSerializable {
     return AuthInfoSerializable(
         accessToken = this.accessToken,
